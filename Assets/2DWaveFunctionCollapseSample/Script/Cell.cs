ad52f9e2d1313943a1f078887d073691e8644528
QinMo <994674480@qq.com>
2024-07-09 22:24:32 +0800

    first commit

---
+++ Assets/2DWaveFunctionCollapseSample/Script/Cell.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public bool collapsed;
+    public Tile[] tileOptions;
+
+    public void CreateCell(bool collapseState, Tile[] tiles)
+    {
+        collapsed = collapseState;
+        tileOptions = tiles;
+    }
+
+    public void RecreateCell(Tile[] tiles)
+    {
+        tileOptions = tiles;
+    }
+}
---
+++ Assets/2DWaveFunctionCollapseSample/Script/Tile.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public class Tile : MonoBehaviour
+{
+    public Tile[] upNeighbors;
+    public Tile[] downNeighbors;
+    public Tile[] leftNeighbors;
+    public Tile[] rightNeighbors;
+}
